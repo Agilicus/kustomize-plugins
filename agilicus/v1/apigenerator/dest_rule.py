9f72c8bc9bbf5a734c57b33d621eda9bd9a72537
Don Bowman <don@agilicus.com>
2019-09-06 09:43:50 -0400

    feat: enable outlier detection
    See [doc](https://istio.io/docs/reference/config/networking/v1alpha3/destination-rule/#OutlierDetection)
    
    The problem we are addressing... if a node goes out of service, it
    takes some time before the controller detects this Pod is gone.
    During this time Envoy blackholes traffic.
    
    Set Outlier as:
    
        outlierDetection:
          consecutiveErrors: 2
          interval: 5s
          baseEjectionTime: 10m
          maxEjectionPercent: 50
    
    meaning...
    
    if we get 2 consecutive 50x (because Envoy cannot connect),
    over 5s, then we kick it out for 10m (multiplied by the number
    of times we have kicked it out!), but only up to 50% (1/2)
    upstream.

---
+++ agilicus/v1/apigenerator/dest_rule.py
@@ -0,0 +1,16 @@
+dest_rule = """
+
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: {cfg[name_version]}-{cfg[name]}-api-dr
+  namespace: {cfg[metadata][namespace]}
+spec:
+  host: {cfg[name_version]}-{cfg[name]}.api.svc.cluster.local
+  trafficPolicy:
+    outlierDetection:
+      consecutiveErrors: 2
+      interval: 5s
+      baseEjectionTime: 10m
+      maxEjectionPercent: 50
+"""
