49653f8af7bda33ab78696a52d3414359ba7b67d
Ross Crawford-d'Heureuse <ross.crawford@mindcurv.com>
2019-10-07 13:12:51 +0200

    Base changes

---
+++ agilicus/v2/secretgenerator/config.py
@@ -0,0 +1,8 @@
+config = """
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: {cfg[metadata][name]}
+data: []
+"""
---
+++ agilicus/v2/secretgenerator/secret.py
@@ -0,0 +1,9 @@
+secret = """
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: {cfg[metadata][name]}
+type: Opaque
+stringData: []
+"""
