05986fe19973bf14865ae108b3fbc7da60d5effd
Don Bowman <don@agilicus.com>
2021-05-11 08:21:58 -0400

    feat: add PodDisruptionBudget, Spread topology constraint
    set minAvailable = 1
    use spread to split by zone

---
+++ agilicus/v1/apigenerator/poddisruptionbudget.py
@@ -0,0 +1,13 @@
+poddisruptionbudget = """
+---
+apiVersion: policy/v1
+kind: PodDisruptionBudget
+metadata:
+  name: {cfg[name_version]}-{cfg[name]}
+  namespace: {cfg[metadata][namespace]}
+spec:
+  minAvailable: 1
+  selector:
+    matchLabels:
+      app: {cfg[name_version]}-{cfg[name]}
+"""
